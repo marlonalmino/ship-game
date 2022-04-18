4632e9e46ea236f20629d1b70206089815d07a71
Marlon Almino <alminomarlon@gmail.com>
2022-04-18 18:01:35 -0300

    implemented function disparo( )

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@ function start() {
   $('#fundoGame').append("<div id='amigo' class='anima3'></div>")
 
   // Principais variáveis do jogo
+
+  var podeAtirar = true
   var jogo = {}
   var velocidade = 5
   var posicaoY = parseInt(Math.random() * 334)
@@ -66,7 +68,7 @@ function start() {
     }
 
     if (jogo.pressionou[TECLA.D]) {
-      //Chama função Disparo
+      disparo()
     }
   }
 
@@ -100,6 +102,36 @@ function start() {
 
     if (posicaoX > 906) {
       $('#amigo').css('left', 0)
+    }
+  }
+
+  //Função que serve para disparar um projétil no jogo
+  function disparo() {
+    if (podeAtirar == true) {
+      podeAtirar = false
+
+      topo = parseInt($('#jogador').css('top'))
+      posicaoX = parseInt($('#jogador').css('left'))
+      tiroX = posicaoX + 190
+      topoTiro = topo + 37
+      $('#fundoGame').append("<div id='disparo'></div")
+      $('#disparo').css('top', topoTiro)
+      $('#disparo').css('left', tiroX)
+
+      var tempoDisparo = window.setInterval(executaDisparo, 30)
+    }
+
+    //Função para executar o disparo
+    function executaDisparo() {
+      posicaoX = parseInt($('#disparo').css('left'))
+      $('#disparo').css('left', posicaoX + 15)
+
+      if (posicaoX > 900) {
+        window.clearInterval(tempoDisparo)
+        tempoDisparo = null
+        $('#disparo').remove()
+        podeAtirar = true
+      }
     }
   }
 }
