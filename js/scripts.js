4ab81d7066265ba973722e0d4977f49a47e6e0b4
Marlon Almino <alminomarlon@gmail.com>
2022-04-18 17:36:00 -0300

    implemented functions loop( ) and movefundo( )

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,19 @@ function start() {
   $('#fundoGame').append("<div id='inimigo1' class='anima2'></div>")
   $('#fundoGame').append("<div id='inimigo2'></div>")
   $('#fundoGame').append("<div id='amigo' class='anima3'></div>")
+
+  var jogo = {}
+
+  //Game Loop
+  jogo.timer = setInterval(loop, 30)
+
+  function loop() {
+    movefundo()
+  }
+
+  //Função que movimenta o fundo do jogo
+  function movefundo() {
+    esquerda = parseInt($('#fundoGame').css('background-position'))
+    $('#fundoGame').css('background-position', esquerda - 1)
+  }
 }
