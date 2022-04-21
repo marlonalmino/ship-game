427e3df5050a9e8b69ecd96ed2af617177d42919
Marlon Almino <alminomarlon@gmail.com>
2022-04-21 18:51:39 -0300

    increasing difficulty

--- js/scripts.js
+++ js/scripts.js
@@ -187,6 +187,7 @@ function start() {
 
     // Disparo com o inimigo1
     if (colisao3.length > 0) {
+      velocidade += 0.3
       pontos = pontos + 100
       inimigo1X = parseInt($('#inimigo1').css('left'))
       inimigo1Y = parseInt($('#inimigo1').css('top'))
