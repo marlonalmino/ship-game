6c57c4eabf0c8c3026c3a8cc9b93f6fe9a8e3393
Marlon Almino <alminomarlon@gmail.com>
2022-04-21 18:44:37 -0300

    added score

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,15 @@ function start() {
   $('#fundoGame').append("<div id='inimigo1' class='anima2'></div>")
   $('#fundoGame').append("<div id='inimigo2'></div>")
   $('#fundoGame').append("<div id='amigo' class='anima3'></div>")
+  $('#fundoGame').append("<div id='placar'></div>")
 
   // Principais variáveis do jogo
 
   var podeAtirar = true
   var fimdejogo = false
+  var pontos = 0
+  var salvos = 0
+  var perdidos = 0
   var jogo = {}
   var velocidade = 5
   var posicaoY = parseInt(Math.random() * 334)
@@ -41,6 +45,7 @@ function start() {
     moveinimigo2()
     moveamigo()
     colisao()
+    placar()
   }
 
   //Função que movimenta o fundo do jogo
@@ -170,12 +175,14 @@ function start() {
 
     // jogador com o amigo
     if (colisao5.length > 0) {
+      salvos++
       reposicionaAmigo()
       $('#amigo').remove()
     }
 
     // Disparo com o inimigo1
     if (colisao3.length > 0) {
+      pontos = pontos + 100
       inimigo1X = parseInt($('#inimigo1').css('left'))
       inimigo1Y = parseInt($('#inimigo1').css('top'))
 
@@ -189,6 +196,7 @@ function start() {
 
     // Disparo com o inimigo2
     if (colisao4.length > 0) {
+      pontos = pontos + 50
       inimigo2X = parseInt($('#inimigo2').css('left'))
       inimigo2Y = parseInt($('#inimigo2').css('top'))
       $('#inimigo2').remove()
@@ -201,6 +209,7 @@ function start() {
 
     //Inimigo2 com o amigo
     if (colisao6.length > 0) {
+      perdidos++
       amigoX = parseInt($('#amigo').css('left'))
       amigoY = parseInt($('#amigo').css('top'))
       explosao3(amigoX, amigoY)
@@ -286,4 +295,17 @@ function start() {
       }
     }
   } // Fim da função reposicionaAmigo()
+
+  //Mostra a pontuação do jogador
+  function placar() {
+    $('#placar').html(
+      '<h2> Pontos: ' +
+        pontos +
+        ' Salvos: ' +
+        salvos +
+        ' Perdidos: ' +
+        perdidos +
+        '</h2>',
+    )
+  } //fim da função placar()
 }
